b12addf4a47f39b97ee97211319453f29633063b
matveyprupas <matveyprupas@gmail.com>
2021-06-08 22:38:55 +0300

    fix: add choose workspace

--- wiring_diagram/scripts/workspace.js
+++ wiring_diagram/scripts/workspace.js
@@ -1,19 +1,59 @@
 const A4_WIDTH = 210;
-const A4_HEIGHT = 297;
 const A3_WIDTH = 297;
 const A3_HEIGHT = 420;
 const GRID_CELL_WIDTH = 2;
 
 
-let workspaceFormat; 
+let workspace = document.querySelector(".workspace");
+let workspaceFormat, workspaceFormatSelect;
 let chooseFormatBtn = document.querySelector(".format input");
-console.log(chooseFormatBtn);
+
+window.addEventListener("resize", setHeightWorkspace);
 chooseFormatBtn.addEventListener("click", chooseFormat);
 
-function chooseFormat (e) {
-    e.preventDefault();
-    workspaceFormat = document.querySelector(".format select"); 
-    console.log(workspaceFormat.selectedOptions[0].value);
+
+function setHeightWorkspace () {
+    workspace.style.height = (document.documentElement.clientHeight - document.querySelector("header.header").offsetHeight) + "px";
 }
+setHeightWorkspace();
+
+
+function chooseFormat (e) {
+    if(e) {
+        e.preventDefault();
+    }
+    workspaceFormatSelect = document.querySelector(".format select");
+    workspaceFormat = document.querySelector(".workspace__format");
+    switch (workspaceFormatSelect.selectedOptions[0].value) {
+        case "A4_portrait":
+            workspaceFormat.style.height = A4_WIDTH + "mm";
+            workspaceFormat.style.width = A3_WIDTH + "mm";
+            break;
+        
+        case "A4_landscape":
+            workspaceFormat.style.height = A3_WIDTH + "mm";
+            workspaceFormat.style.width = A4_WIDTH + "mm";
+            break;
+        
+        case "A3_portrait":
+            workspaceFormat.style.height = A3_WIDTH + "mm";
+            workspaceFormat.style.width = A3_HEIGHT + "mm";
+            break;
+
+        case "A3_landscape":
+            workspaceFormat.style.height = A3_HEIGHT + "mm";
+            workspaceFormat.style.width = A3_WIDTH + "mm";
+            break;
+    
+    }
+
+    workspaceFormatWidth = workspaceFormat.offsetWidth;
+    workspaceFormatHeight = workspaceFormat.offsetHeight;
+}
+
+chooseFormat();
+
+
+
 
 
